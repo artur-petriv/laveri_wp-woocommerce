ccb0d5b18f0363b84dd67b06ee72986d14e4e3a5
Artur Petriv <artur.petriv@gmail.com>
2020-11-05 18:15:42 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+	<meta charset="utf-8">
+	<title>LAVERI - accessories and bags brand shop</title>
+	<meta content="width=device-width, initial-scale=1" name="viewport">
+	<meta content="ie=edge" http-equivx="x-ua-compatible">
+	<link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&amp;display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.min.css">
+</head>
+
+<body>
+	<header class="header">
+		<div class="header__bar">
+			<div class="container">
+				<div class="header__bar-wrap">
+					<div class="search">
+						<form class="search__form"><input class="search__input" type="text" name="search" method="get"
+								placeholder="Искать"><button class="search__submit" type="submit"><svg
+									class="icon-magnifier search__icon">
+									<use xlink:href="img/sprite.svg#magnifier"></use>
+								</svg></button></form>
+					</div>
+					<div class="header__content">
+						<ul class="header__socials">
+							<li class="header__social"><a class="header__social-link" href="#"><svg
+										class="icon-facebook header__social-icon">
+										<use xlink:href="img/sprite.svg#facebook"></use>
+									</svg></a></li>
+							<li class="header__social"><a class="header__social-link" href="#"><svg
+										class="icon-vk header__social-icon">
+										<use xlink:href="img/sprite.svg#vk"></use>
+									</svg></a></li>
+							<li class="header__social"><a class="header__social-link" href="#"><svg
+										class="icon-instagram header__social-icon">
+										<use xlink:href="img/sprite.svg#instagram"></use>
+									</svg></a></li>
+							<li class="header__social"><a class="header__social-link" href="#"><svg
+										class="icon-phone header__social-icon">
+										<use xlink:href="img/sprite.svg#phone"></use>
+									</svg></a></li>
+						</ul><a class="header__cart cart-header" href="#"><svg class="icon-cart header__cart-icon">
+								<use xlink:href="img/sprite.svg#cart"></use>
+							</svg><span class="cart-header__title">Ваша корзина</span><span class="cart-header__dash">-</span><span
+								class="cart-header__amount">0 грн.</span></a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="header__menu">
+			<div class="container">
+				<nav class="header__nav nav-header">
+					<ul class="nav-header__list">
+						<li class="nav-header__item"><a class="nav-header__link" href="./brand.html">
+								<h4 class="nav-header__title">О бренде</h4>
+							</a></li>
+						<li class="nav-header__item"><a class="nav-header__link" href="./catalog.html">
+								<h4 class="nav-header__title">Каталог</h4>
+							</a></li>
+						<li class="nav-header__item"><a class="nav-header__link" href="./"><svg class="icon-logo header__logo">
+									<use xlink:href="img/sprite.svg#logo"></use>
+								</svg></a></li>
+						<li class="nav-header__item"><a class="nav-header__link" href="./contacts.html">
+								<h4 class="nav-header__title">Контакты</h4>
+							</a></li>
+						<li class="nav-header__item"><a class="nav-header__link" href="./delivery.html">
+								<h4 class="nav-header__title">Доставка</h4>
+							</a></li>
+						<li class="nav-header__mobile"><button class="hamburger hamburger--spin" type="button"><span
+									class="hamburger-box"><span class="hamburger-inner"></span></span></button></li>
+					</ul>
+				</nav>
+				<div class="nav-mob">
+					<div class="nav-mob__wrap">
+						<ul class="nav-mob__list">
+							<li class="nav-mob__item"><a class="nav-mob__link" href="./">Главная</a></li>
+							<li class="nav-mob__item"><a class="nav-mob__link" href="./catalog.html">Каталог</a></li>
+							<li class="nav-mob__item"><a class="nav-mob__link" href="./contacts.html">Контакты</a></li>
+							<li class="nav-mob__item"><a class="nav-mob__link" href="./delivery.html">Доставка</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	<div class="slider-main">
+		<div class="container">
+			<div class="swiper-container">
+				<div class="swiper-wrapper">
+					<div class="slider-main__item swiper-slide"><img class="slider-main__img" src="img/banner_01.jpg" alt="Bag"><a
+							class="slider-main__link" href="./card.html">Подробнее<svg class="icon-arrow-right slider-main__icon">
+								<use xlink:href="img/sprite.svg#arrow-right"></use>
+							</svg></a></div>
+					<div class="slider-main__item swiper-slide"><img class="slider-main__img" src="img/banner_02.jpg" alt="Bag"><a
+							class="slider-main__link" href="./card.html">Подробнее<svg class="icon-arrow-right slider-main__icon">
+								<use xlink:href="img/sprite.svg#arrow-right"></use>
+							</svg></a></div>
+					<div class="slider-main__item swiper-slide"><img class="slider-main__img" src="img/banner_03.jpg" alt="Bag"><a
+							class="slider-main__link" href="./card.html">Подробнее<svg class="icon-arrow-right slider-main__icon">
+								<use xlink:href="img/sprite.svg#arrow-right"></use>
+							</svg></a></div>
+				</div>
+				<div class="swiper-pagination"></div>
+				<div class="swiper-button-prev"></div>
+				<div class="swiper-button-next"></div>
+			</div>
+		</div>
+	</div>
+	<section class="featured-products">
+		<div class="container">
+			<h2 class="featured-products__title">Featured Products</h2>
+			<div class="featured-products__list">
+				<div class="product"><a class="product__link" href="./card.html">
+						<div class="product__images">
+							<div class="product__image product__image-front"><img class="product__img" src="img/product_01.jpg"
+									alt="Product"></div>
+							<div class="product__image product__image-back"><img class="product__img" src="img/product_02.jpg"
+									alt="Product"></div>
+						</div>
+					</a><a class="product__link" href="./card.html">
+						<h3 class="product__title">Сумка Bordon синий крокодил</h3>
+					</a>
+					<div class="product__rate"><span class="product__rate-text">Not rated</span></div>
+					<div class="product__price">2500 грн.</div><a class="product__button">В корзину</a>
+				</div>
+				<div class="product"><a class="product__link" href="./card.html">
+						<div class="product__images">
+							<div class="product__image product__image-front"><img class="product__img" src="img/product_01.jpg"
+									alt="Product"></div>
+							<div class="product__image product__image-back"><img class="product__img" src="img/product_02.jpg"
+									alt="Product"></div>
+						</div>
+					</a><a class="product__link" href="./card.html">
+						<h3 class="product__title">Сумка Bordon синий крокодил</h3>
+					</a>
+					<div class="product__rate"><span class="product__rate-text">Not rated</span></div>
+					<div class="product__price">2500 грн.</div><a class="product__button">В корзину</a>
+				</div>
+				<div class="product"><a class="product__link" href="./card.html">
+						<div class="product__images">
+							<div class="product__image product__image-front"><img class="product__img" src="img/product_01.jpg"
+									alt="Product"></div>
+							<div class="product__image product__image-back"><img class="product__img" src="img/product_02.jpg"
+									alt="Product"></div>
+						</div>
+					</a><a class="product__link" href="./card.html">
+						<h3 class="product__title">Сумка Bordon синий крокодил</h3>
+					</a>
+					<div class="product__rate"><span class="product__rate-text">Not rated</span></div>
+					<div class="product__price">2500 грн.</div><a class="product__button">В корзину</a>
+				</div>
+				<div class="product"><a class="product__link" href="./card.html">
+						<div class="product__images">
+							<div class="product__image product__image-front"><img class="product__img" src="img/product_01.jpg"
+									alt="Product"></div>
+							<div class="product__image product__image-back"><img class="product__img" src="img/product_02.jpg"
+									alt="Product"></div>
+						</div>
+					</a><a class="product__link" href="./card.html">
+						<h3 class="product__title">Сумка Bordon синий крокодил</h3>
+					</a>
+					<div class="product__rate"><span class="product__rate-text">Not rated</span></div>
+					<div class="product__price">2500 грн.</div><a class="product__button">В корзину</a>
+				</div>
+				<div class="product"><a class="product__link" href="./card.html">
+						<div class="product__images">
+							<div class="product__image product__image-front"><img class="product__img" src="img/product_01.jpg"
+									alt="Product"></div>
+							<div class="product__image product__image-back"><img class="product__img" src="img/product_02.jpg"
+									alt="Product"></div>
+						</div>
+					</a><a class="product__link" href="./card.html">
+						<h3 class="product__title">Сумка Bordon синий крокодил</h3>
+					</a>
+					<div class="product__rate"><span class="product__rate-text">Not rated</span></div>
+					<div class="product__price">2500 грн.</div><a class="product__button">В корзину</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	<footer class="footer">
+		<div class="container">
+			<div class="footer__wrap">
+				<div class="footer__content content-footer">
+					<div class="content-footer__column">
+						<h4 class="content-footer__title">Laveri</h4>
+						<ul class="content-footer__list">
+							<li class="content-footer__item"><a class="content-footer__link" href="./brand.html">О бренде</a></li>
+							<li class="content-footer__item"><a class="content-footer__link" href="./contacts.html">Контакты</a></li>
+							<li class="content-footer__item"><a class="content-footer__link" href="./agreement.html">Условия
+									использования</a></li>
+						</ul>
+					</div>
+					<div class="content-footer__column">
+						<h4 class="content-footer__title">Покупателям</h4>
+						<ul class="content-footer__list">
+							<li class="content-footer__item"><a class="content-footer__link" href="./delivery.html">Оплата и
+									доставка</a></li>
+							<li class="content-footer__item"><a class="content-footer__link" href="./faq.html">FAQ</a></li>
+						</ul>
+					</div>
+					<div class="content-footer__column">
+						<h4 class="content-footer__title">Категории товаров</h4>
+						<ul class="content-footer__list">
+							<li class="content-footer__item"><a class="content-footer__link content-footer__link_category"
+									href="./catalog.html">Сумки</a></li>
+						</ul>
+					</div>
+					<div class="content-footer__column">
+						<h4 class="content-footer__title">Подписывайтесь</h4>
+						<ul class="content-footer__list content-footer__list_socials">
+							<li class="content-footer__item content-footer__item_social"><a class="content-footer__link" href="#"><svg
+										class="icon-facebook content-footer__icon">
+										<use xlink:href="img/sprite.svg#facebook"></use>
+									</svg></a></li>
+							<li class="content-footer__item content-footer__item_social"><a class="content-footer__link" href="#"><svg
+										class="icon-instagram content-footer__icon">
+										<use xlink:href="img/sprite.svg#instagram"></use>
+									</svg></a></li>
+							<li class="content-footer__item content-footer__item_social"><a class="content-footer__link" href="#"><svg
+										class="icon-vk content-footer__icon">
+										<use xlink:href="img/sprite.svg#vk"></use>
+									</svg></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="footer__copyright"><span class="footer__text">© 2020 Laveri</span></div>
+			</div>
+		</div>
+	</footer>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script src="js/main.min.js"></script>
+</body>
+
+</html>
